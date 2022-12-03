853ebc7e9e2eb2ea152d4418b4ef2b84ab8caa6c
luisrojas1mt <luis.rojas20.larv@gmail.com>
2022-12-03 17:11:38 -0500

    segundo commit

---
+++ src/componentes/Admin.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Admin = () => { 
+
+  return (
+      <main className='container mx-auto mt-5 md:mt-20 p-5 md:flex md:justify-center'>
+        <div className='md:w-2/3 lg:w-2/5'>
+            <h1 className="inline bg-gradient-to-r from-indigo-200 via-violet-700 to-indigo-200 bg-clip-text font-display text-5xl tracking-tight text-transparent">
+                Panel de Administrador 4
+            </h1>            
+              
+        </div>
+      </main>
+    );
+}
+
+export default Admin;
